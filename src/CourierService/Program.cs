d8bda1466238f74f00d6c5f74b6d22aa42ad1004
Albert Hives <ahives@hotmail.com>
2020-12-24 19:11:12 -0800

    Created courier service and moved appropriate code.

---
+++ src/CourierService/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace CourierService
+{
+    using System;
+    using System.IO;
+    using System.Reflection;
+    using System.Threading.Tasks;
+    using Consumers;
+    using MassTransit;
+    using MassTransit.EntityFrameworkCoreIntegration;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Hosting;
+    using Serilog;
+    using Serilog.Events;
+    using StateMachines;
+    using StateMachines.Sagas;
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await CreateHostBuilder(args).Build().RunAsync();
+        }
+
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder()
+                .UseSerilog((host, log) =>
+                {
+                    string? appBin = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+                    log.MinimumLevel.Information();
+                    log.WriteTo.File($"{appBin}/log/log-{DateTime.Now:yyMMdd_HHmmss}.txt");
+                    log.WriteTo.Console(LogEventLevel.Debug);
+                })
+                .ConfigureAppConfiguration((host, builder) =>
+                {
+                    builder.AddJsonFile("appsettings.json", false);
+                })
+                .ConfigureServices((host, services) =>
+                {
+                    services.AddMassTransit(x =>
+                    {
+                        x.AddConsumer<CourierDispatchConsumer>();
+                        
+                        x.SetKebabCaseEndpointNameFormatter();
+                        
+                        x.UsingRabbitMq((context, cfg) =>
+                        {
+                            string vhost = host.Configuration
+                                .GetSection("Application")
+                                .GetValue<string>("VirtualHost");
+                            
+                            cfg.Host("localhost", vhost, h =>
+                            {
+                                h.Username("guest");
+                                h.Password("guest");
+                            });
+                            
+                            cfg.ConfigureEndpoints(context);
+                            // cfg.UseMessageRetry(x => x.SetRetryPolicy(new RetryPolicyFactory()));
+                        });
+
+                        x.AddSagaStateMachine<CourierStateMachine, CourierState>()
+                            .EntityFrameworkRepository(r =>
+                            {
+                                r.ConcurrencyMode = ConcurrencyMode.Pessimistic;
+                                
+                                r.AddDbContext<DbContext, CourierStateDbContext>((provider, builder) =>
+                                {
+                                    builder.UseNpgsql(host.Configuration.GetConnectionString("OrdersConnection"), m =>
+                                    {
+                                        m.MigrationsAssembly(Assembly.GetExecutingAssembly().GetName().Name);
+                                        m.MigrationsHistoryTable($"__{nameof(CourierStateDbContext)}");
+                                    });
+                                });
+                            });
+                    });
+
+                    services.AddMassTransitHostedService();
+                });
+    }
+}
