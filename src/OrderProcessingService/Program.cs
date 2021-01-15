1229fc949d15c62729071457d6cc54fb9837718c
Albert Hives <ahives@hotmail.com>
2021-01-15 12:37:08 -0800

    added scaffolding for kitchen gRPC service

---
+++ src/OrderProcessingService/Program.cs
@@ -0,0 +1,97 @@
+﻿namespace OrderProcessingService
+{
+    using System;
+    using System.IO;
+    using System.Reflection;
+    using System.Threading.Tasks;
+    using Core.Consumers;
+    using Core.StateMachines;
+    using Core.StateMachines.Sagas;
+    using Data.Core;
+    using MassTransit;
+    using MassTransit.EntityFrameworkCoreIntegration;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Serilog;
+    using Serilog.Events;
+    using Service.Grpc.Core;
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await CreateHostBuilder(args).RunConsoleAsync();
+        }
+
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder()
+                .UseSerilog((host, log) =>
+                {
+                    string? appBin = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+                    log.MinimumLevel.Information();
+                    log.WriteTo.File($"{appBin}/log/log-{DateTime.Now:yyMMdd_HHmmss}.txt");
+                    log.WriteTo.Console(LogEventLevel.Information);
+                })
+                .ConfigureAppConfiguration((host, config) =>
+                {
+                    config.Sources.Clear();
+                    config.AddJsonFile("appsettings.json", false);
+                })
+                .ConfigureServices((host, services) =>
+                {
+                    // services.AddScoped<IShelfManager, ShelfManager>();
+                    services.AddSingleton<IGrpcClient<IOrderProcessor>, OrderProcessorClient>();
+
+                    services.AddDbContext<OrderProcessingServiceDbContext>(builder =>
+                        builder.UseNpgsql(host.Configuration.GetConnectionString("OrdersConnection"), m =>
+                        {
+                            m.MigrationsAssembly(Assembly.GetExecutingAssembly().GetName().Name);
+                            m.MigrationsHistoryTable($"__{nameof(OrderProcessingServiceDbContext)}");
+                        }));
+                    
+                    services.AddMassTransit(x =>
+                    {
+                        x.AddConsumer<PrepareOrderItemConsumer>();
+                        
+                        x.SetKebabCaseEndpointNameFormatter();
+                        
+                        x.UsingRabbitMq((context, cfg) =>
+                        {
+                            string vhost = host.Configuration
+                                .GetSection("Application")
+                                .GetValue<string>("VirtualHost");
+                            
+                            cfg.Host("localhost", vhost, h =>
+                            {
+                                h.Username("guest");
+                                h.Password("guest");
+                            });
+                            
+                            cfg.ConfigureEndpoints(context);
+                            // cfg.UseMessageRetry(x => x.SetRetryPolicy(new RetryPolicyFactory()));
+                        });
+
+                        x.AddSagaStateMachine<OrderStateMachine, OrderState>()
+                            .EntityFrameworkRepository(r =>
+                            {
+                                r.ConcurrencyMode = ConcurrencyMode.Optimistic;
+                                r.LockStatementProvider = new PostgresLockStatementProvider();
+                                r.ExistingDbContext<OrderProcessingServiceDbContext>();
+                            });
+                        
+                        x.AddSagaStateMachine<OrderItemStateMachine, OrderItemState>()
+                            .EntityFrameworkRepository(r =>
+                            {
+                                r.ConcurrencyMode = ConcurrencyMode.Optimistic;
+                                r.LockStatementProvider = new PostgresLockStatementProvider();
+                                r.ExistingDbContext<OrderProcessingServiceDbContext>();
+                            });
+                    });
+
+                    services.AddMassTransitHostedService();
+                });
+    }
+}
